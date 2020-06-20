20f2f4f3dc93308fad76a30b1060ba6bf5d17be2
Zahraa Zuhair L <30442334+ProZahraaZuhaier@users.noreply.github.com>
2020-06-20 23:09:06 +0300

    Add files via upload

---
+++ home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Home Screen',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
